b6e35e75b5ae1828614b1fe11645c8a6c5691a4d
ederose12 <ederose12@gmail.com>
2017-07-18 16:02:21 -0400

    Create shapes.py

---
+++ shapes.py
@@ -0,0 +1,28 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = -250
+y_pos = -150
+
+### Write your code below:
+while True:
+        pendown()
+        forward(50)
+        left (120)
+        penup()
+
+while True:
+    pendown()
+    forward(100)
+    left(90)
+    penup()
+    
+
+
+# Close window on click.
+exitonclick()
